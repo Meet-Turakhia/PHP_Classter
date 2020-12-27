e70459d6e94e71e487dc9f460fdac957b5da550a
Meet-Turakhia <turakhiameeturesh@gmail.com>
2020-12-27 10:27:56 +0530

    modifying footer

--- index.php
+++ index.php
@@ -361,12 +361,8 @@
             <hr class="rgba-white-light" style="margin: 0 15%;">
             <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
                 <div class="col-md-8 col-12 mt-5">
-                    <p style="line-height: 1.7rem">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                        accusantium doloremque laudantium, totam rem
-                        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-                        explicabo.
-                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-                        consequuntur.</p>
+                    <p style="line-height: 1.7rem">Classter is a virtual classroom management sytem with college hierarchy which lets
+                        teachers make classes, the viewers can view them, the students can login and vew their details and admin and hod have respective powers</p>
                 </div>
             </div>
             <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
